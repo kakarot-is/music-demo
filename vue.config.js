35aca85435e71bed9a0b78e6fc554cdc1ef1631a
kakarot <xc18279636216@126.com>
2022-04-08 11:55:28 +0800

    test

--- vue.config.js
+++ vue.config.js
@@ -13,18 +13,18 @@ module.exports = {
             }
         }
     },
-    devServer: {
-        proxy: {//解决跨域问题
-            '/api': {
-                // 此处的写法，目的是为了 将 /api 替换成 https://autumnfish.cn/
-                target: 'https://autumnfish.cn/',
-                // 允许跨域
-                changeOrigin: true,
-                ws: true,
-                pathRewrite: {
-                    '^/api': ''
-                }
-            }
-        }
-    }
+    // devServer: {
+    //     proxy: {//解决跨域问题
+    //         '/api': {
+    //             // 此处的写法，目的是为了 将 /api 替换成 https://autumnfish.cn/
+    //             target: 'https://autumnfish.cn/',
+    //             // 允许跨域
+    //             changeOrigin: true,
+    //             ws: true,
+    //             pathRewrite: {
+    //                 '^/api': ''
+    //             }
+    //         }
+    //     }
+    // }
 };
